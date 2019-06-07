26e3d796a1c7489e168815634fcd0b7993027a28
Mada Gower <gmgower@gmail.com>
2019-06-07 09:43:32 -0500

    Add wheel, copy, & dblclick.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 // Your code goes here
 
-// mouse events
+// mouse click events
 const funInTheSunBtn =  document.querySelector('.btn');
 funInTheSunBtn.addEventListener('click', function (event) {
   event.target.style.color = "red"
@@ -13,8 +13,25 @@ introImg.forEach(function (element) {
   element.addEventListener('mouseover', function () {
     element.style.borderRadius = '100px';
   })
-  element.addEventListener('mouseout', function () {
-    element.style.borderRadius = '50px';
+  element.addEventListener('mouseout', function (e) {
+    element.style.borderRadius = '10px';
+    e.preventDefault();
   })
+})
+
+// wheel, copy, dblclick
+const body = document.querySelector('body')
+
+body.addEventListener('wheel', function (e) {
+  e.target.style.backgroundColor = 'red';
+})
+
+body.addEventListener('copy', function (e) {
+  e.target.style.visibility = 'hidden';
+  e.preventDefault();
+})
+
+body.addEventListener('dblclick', function (e) {
+  e.target.style.fontSize = '2rem'
 })
 
