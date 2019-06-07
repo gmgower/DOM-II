c14bda5f686cb9bf67d6f293686cc431972f1bea
Mada Gower <gmgower@gmail.com>
2019-06-06 20:12:57 -0500

    Add click, mouseover & mouseout

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
 // Your code goes here
+
+// mouse events
+const funInTheSunBtn =  document.querySelector('.btn');
+funInTheSunBtn.addEventListener('click', function (event) {
+  event.target.style.color = "red"
+  console.log(`funInTheSunBtn clicked: ${event}`)
+})
+
+// mouseover  & mouseout
+const introImg = document.querySelectorAll('.intro img');
+introImg.forEach(function (element) {
+  element.addEventListener('mouseover', function () {
+    element.style.borderRadius = '100px';
+  })
+  element.addEventListener('mouseout', function () {
+    element.style.borderRadius = '50px';
+  })
+})
+
