6534390db07a7b5a7ad160835997e2bcef2d2e37
Mada Gower <gmgower@gmail.com>
2019-06-07 22:09:20 -0500

    Add paste event.

--- js/index.js
+++ js/index.js
@@ -2,29 +2,22 @@
 
 // mouse click events
 const buttons = document.querySelectorAll('.btn');
-buttons[0].addEventListener('click', function (event) {
-  event.target.style.color = `red`
-  console.log(`buttons clicked: ${event}`)
-})
-buttons[1].addEventListener('click', function (event) {
-    event.target.style.fontSize = `3rem`
-  console.log(`buttons clicked: ${event}`)
-    
-  })
+buttons[0].addEventListener('click', e =>  event.target.style.color = `red`)
+buttons[1].addEventListener('click', e =>  event.target.style.fontSize = `3rem`)
+  // console.log(`buttons clicked: ${event}`)
+
 
 // mouseover  & mouseout
 const introImg = document.querySelectorAll('.intro img');
 introImg.forEach(function (element) {
-  element.addEventListener('mouseover', function () {
-    element.style.borderRadius = '100px';
-  })
-  element.addEventListener('mouseout', function (e) {
+  element.addEventListener('mouseover', () => element.style.borderRadius = '100px')
+  element.addEventListener('mouseout', e  => {
     element.style.borderRadius = '10px';
     e.preventDefault();
   })
 })
 
-// wheel, copy, dblclick
+// wheel, copy, dblclick, paste
 const body = document.querySelector('body')
 
 body.addEventListener('wheel', e => e.target.style.backgroundColor = 'lightgray')
@@ -32,6 +25,11 @@ body.addEventListener('wheel', e => e.target.style.backgroundColor = 'lightgray'
 body.addEventListener('copy', e => {
   e.target.style.visibility = 'hidden';
   e.preventDefault();
+})
+
+body.addEventListener('paste', function (event) {
+  let paste = (event.clipboardData || window.clipboardData).getData('text');
+  paste =paste.toUppercase()
 })
 
 body.addEventListener('dblclick', e => e.target.style.fontSize = '2rem')
@@ -53,25 +51,23 @@ destImg.addEventListener('mouseout', e => {
 // drag
 const allImgs = document.querySelectorAll('img')
 
-allImgs[2].addEventListener('drag', function (e) {
-  e.target.style.padding = '30%';
-})
+allImgs[2].addEventListener('drag', e => e.target.style.padding = '30%')
 
 //scroll
 const selectP = document.querySelectorAll('p');
 
-window.addEventListener('scroll', function (e) {
+window.addEventListener('scroll', e => {
   selectP[1].style.border = '1px dashed blue';
   selectP[2].style.border = '2px solid green';
   e.preventDefault()
 })
 
+// propagation check
 let lastClick = document.querySelector('.content-destination');
-lastClick.addEventListener('click', function () {
-  console.log('No logged')
-})
 
-allImgs[3].addEventListener('click', function (e) {
+lastClick.addEventListener('click', () => console.log('No logged'))
+
+allImgs[3].addEventListener('click', e => {
   console.log('logged');
   e.stopPropagation();
 })
